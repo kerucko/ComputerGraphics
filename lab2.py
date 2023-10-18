4bd4df24463bdb7de6bf6cad6c5181dda4c9ccc7
kerucko <cykinpavel@gmail.com>
2023-10-18 23:52:18 +0300

    lab2

---
+++ lab2.py
@@ -0,0 +1,27 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+
+fig = plt.figure()
+ax = fig.add_subplot(projection="3d")
+
+x1, y1, z1 = [0, 2, 1, 0], [0, 0, np.sqrt(3), 0], [-1, -1, -1, -1]
+x2, y2, z2 = [0, 2, 1, 0], [0, 0, np.sqrt(3), 0], [1, 1, 1, 1]
+x3, y3, z3 = [0, 0], [0, 0], [1, -1]
+x4, y4, z4 = [2, 2], [0, 0], [1, -1]
+x5, y5, z5 = [1, 1], [np.sqrt(3), np.sqrt(3)], [-1, 1]
+
+ax.scatter(x1, y1, z1, c='blue', s=100)
+ax.scatter(x2, y2, z2, c='green', s=100)
+ax.scatter(x3, y3, z3, c='yellow', s=100)
+ax.scatter(x4, y4, z4, c='cyan', s=100)
+ax.scatter(x5, y5, z5, c='magenta', s=100)
+
+ax.plot(x1, y1, z1, color='black')
+ax.plot(x2, y2, z2, color='black')
+ax.plot(x3, y3, z3, color='black')
+ax.plot(x4, y4, z4, color='black')
+ax.plot(x5, y5, z5, color='black')
+
+plt.show()
